8bbb1350581a1f96f58a8e742c82cd2b33028b41
Ubuntu <ubuntu@ip-172-31-29-22.us-east-2.compute.internal>
2019-09-24 14:09:51 +0000

    Day 1 figuring stuff out

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies
+  resources :movies do
+    Movies.order(:title)
   
   # Example resource route with options:
   #   resources :products do
